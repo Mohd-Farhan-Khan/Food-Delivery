abb5b5d9789cbfe4b1c21af9952a83137c55b309
Mohd Farhan Khan <goldenstone447@gmail.com>
2025-08-07 23:00:45 +0530

    feat: enhance Cart and PlaceOrder components with total calculation and styling

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,11 +1,55 @@
 import React from 'react'
 import './PlaceOrder.css'
+import { useContext } from 'react'
+import { StoreContext } from '../../context/StoreContext'
 
 const PlaceOrder = () => {
+
+  const { getTotalCartAmount } = useContext(StoreContext);
+
   return (
-    <div>
-      
-    </div>
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First name" required />
+          <input type="text" placeholder="Last name" required />
+        </div>
+        <input type="text" placeholder="Email address" required />
+        <input type="text" placeholder="Street" required />
+        <div className="multi-fields">
+          <input type="text" placeholder="City" required />
+          <input type="text" placeholder="State" required />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder="Zip Code" required />
+          <input type="text" placeholder="Country" required />
+        </div>
+        <input type="text" placeholder="Phone number" required />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Total</p>
+              <p>${getTotalCartAmount() + (getTotalCartAmount() === 0 ? 0 : 2)}</p>
+            </div>
+          </div>
+          <button>PROCEED TO PAYMENT</button>
+        </div>
+      </div>
+    </form>
   )
 }
 
