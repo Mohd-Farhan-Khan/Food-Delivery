8199cb8efa6402f545446030b5d62be2fbc99417
Mohd Farhan Khan <goldenstone447@gmail.com>
2025-08-10 01:46:58 +0530

    feat: pass API URL as a prop to Add and List components; implement food list fetching and removal functionality

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './List.css'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
-const List = () => {
+const List = ({ url }) => {
+
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error("Failed to fetch list");
+    }
+  }
+
+  const removeFood = async (foodId) => {
+    const response = await axios.delete(`${url}/api/food/remove`, { data: { id: foodId } });
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message);
+    } else {
+      toast.error("Failed to remove food");
+    }
+  }
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
   return (
-    <div>
-        <h1>List of items</h1>
+    <div className="list add flex-col">
+      <p>All foods list</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item, index) => {
+          return (
+            <div className="list-table-format" key={index}>
+              <img src={`${url}/images/` + item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>${item.price}</p>
+              <p onClick={() => removeFood(item._id)} className='cursor'>Remove</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   )
 }
