fdb5e613b5a125ef429de41c566828bc1187da6d
Mohd Farhan Khan <goldenstone447@gmail.com>
2025-08-10 23:59:43 +0530

    feat: add axios for API calls; implement login and registration functionality in LoginPopup; update FoodItem and Cart components to use dynamic image URLs; enhance Navbar with profile dropdown and logout feature; manage token and food list in StoreContext

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -3,27 +3,27 @@ import './FoodItem.css'
 import { assets } from '../../assets/assets'
 import { StoreContext } from '../../context/StoreContext.jsx';
 
-const FoodItem = ({id, name, price, description, image}) => {
+const FoodItem = ({ id, name, price, description, image }) => {
 
-    const {cartItems, addToCart, removeFromCart} = useContext(StoreContext);
+  const { cartItems, addToCart, removeFromCart, url } = useContext(StoreContext);
 
   return (
     <div className='food-item'>
       <div className="food-item-img-container">
-        <img className='food-item-image' src={image} alt={name} />
+        <img className='food-item-image' src={url + "/images/" + image} alt={name} />
         {!cartItems[id]
-            ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="Add" />
-            : <div className="food-item-counter">
-                <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="" />
-                <p>{cartItems[id]}</p>
-                <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="" />
-              </div>
+          ? <img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="Add" />
+          : <div className="food-item-counter">
+            <img onClick={() => removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+            <p>{cartItems[id]}</p>
+            <img onClick={() => addToCart(id)} src={assets.add_icon_green} alt="" />
+          </div>
         }
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
-            <p>{name}</p>
-            <img src={assets.rating_stars} alt="" />
+          <p>{name}</p>
+          <img src={assets.rating_stars} alt="" />
         </div>
         <p className='food-item-desc'>{description}</p>
         <p className='food-item-price'>${price}</p>
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,12 +1,16 @@
 import { createContext, useState } from 'react';
 import { food_list } from '../assets/assets';
 import { useEffect } from 'react';
+import axios from 'axios';
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
+    const url = "http://localhost:4000";
+    const [token, setToken] = useState("");
+    const [food_list, setFoodList] = useState([]);
 
     const addToCart = (itemId) => {
         if (!cartItems[itemId]) {
@@ -31,13 +35,36 @@ const StoreContextProvider = (props) => {
         return totalAmount;
     }
 
+    const fetchFoodList = async () => {
+        try {
+            const response = await axios.get(url + "/api/food/list");
+            setFoodList(response.data.data);
+        } catch (error) {
+            console.error("Error fetching food list:", error);
+        }
+    }
+
+    // Check for token in localStorage so that when the user refreshes the page, the token is still available and thus the user doesn't have to log in again
+    useEffect(() => {
+        async function loadData() {
+            await fetchFoodList();
+            if (localStorage.getItem("token")) {
+                setToken(localStorage.getItem("token"));
+            }
+        }
+        loadData();
+    }, [])
+
     const contextValue = {
         food_list,
         cartItems,
         setCartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount
+        getTotalCartAmount,
+        url,
+        token,
+        setToken
     }
 
     return (
